5a97050c81454384877f6bd156d0f3bc2825208d
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-04 16:21:24 +0530

    db

--- OOP - luxe hotel/src/jdbc/SendMessageByUser.java
+++ OOP - luxe hotel/src/jdbc/SendMessageByUser.java
@@ -13,3 +13,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.DB_Connection;
+
+/**
+ * Servlet implementation class SendMessageByUser
+ */
+@WebServlet("/SendMessageByUser")
+public class SendMessageByUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SendMessageByUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+        	String current_user_val = request.getParameter("current_user");
+            String Message_val = request.getParameter("Message");
+        	String Email_val = request.getParameter("Email");
+                DB_Connection obj_DB_Connection = new DB_Connection();
+                Connection connection = obj_DB_Connection.get_connection();
+                PreparedStatement ps = null;
