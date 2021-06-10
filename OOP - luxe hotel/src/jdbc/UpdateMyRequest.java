0cc6a603e84d6165ac36a17cf832c9930fa5ddaa
pramodC135 <pramodmadhushan4@gmail.com>
2021-06-11 01:56:28 +0530

    fix

--- OOP - luxe hotel/src/jdbc/UpdateMyRequest.java
+++ OOP - luxe hotel/src/jdbc/UpdateMyRequest.java
@@ -13,3 +13,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.DB_Connection;
+
+
+/**
+ * Servlet implementation class UpdateMyRequest
+ */
+@WebServlet("/UpdateMyRequest")
+public class UpdateMyRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateMyRequest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+        	String req_id_val = request.getParameter("req_id");
+        	String Adults_update_input_val = request.getParameter("Adults_update_input");
+        	String Kids_update_input_val = request.getParameter("Kids_update_input");
+        	String CheckIn_update_input_val = request.getParameter("CheckIn_update_input");
+        	String CheckOut_update_input_val = request.getParameter("CheckOut_update_input");
